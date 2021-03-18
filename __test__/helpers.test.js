86d14af09aba872846fb73c9451e6b85ea47fec0
Yasser <yassergeo@gmail.com>
2021-03-18 14:55:59 +0000

    added faild test

--- __test__/helpers.test.js
+++ __test__/helpers.test.js
@@ -5,6 +5,6 @@ describe('helpers function should run', () => {
     expect(sum(2, 2)).toEqual(4)
   })
   test('should only print strings', () => {
-    expect(printOnlyString('4')).toEqual('4')
+    expect(printOnlyString(4)).toEqual(4)
   })
 })
