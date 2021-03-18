0280620daa7b624193ca415152ea4b23100b5685
Yasser <yassergeo@gmail.com>
2021-03-18 03:22:19 +0000

    Added formats options to prettier

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,16 @@
 const sum = (a, b) => {
-  return a + b;
-};
+  return a + b
+}
 
 const printOnlyString = (newString) => {
-  if (typeof newString == "string") {
-    return newString;
+  if (typeof newString == 'string') {
+    return newString
   } else {
-    return "Only string needed";
+    return 'Only string needed'
   }
-};
+}
 
 module.exports = {
   sum,
   printOnlyString,
-};
+}
