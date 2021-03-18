e462a4bfffb2d3b1f82ad1852f4321357f5cfe2f
Yasser <yassergeo@gmail.com>
2021-03-18 02:13:07 +0000

    Added helper test

--- __test__/helpers.test.js
+++ __test__/helpers.test.js
@@ -1,7 +1,10 @@
-const {sum} = require("./../src/helpers")
+const {sum, printOnlyString} = require("./../src/helpers")
 
 describe("helpers function should run", ()=>{
   test("Should add to numbers",()=>{
     expect(sum(2,2)).toEqual(4)
+  })
+  test("should only print strings",()=>{
+    expect(printOnlyString("4")).toEqual("4")
   })
 })
--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,15 @@ const sum = (a , b) => {
  return a + b
 }
 
+const printOnlyString = (newString) => {
+    if(typeof newString == "string"){
+       return newString
+    }else {
+        return "Only string needed"
+    }
+}
+
 module.exports = {
-    sum
+    sum,
+    printOnlyString
 }
